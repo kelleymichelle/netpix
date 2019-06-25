ff0aa699e3b28f83573c5a635926f19682fd283f
Kelley Chaplain <kmchaplain@gmail.com>
2019-06-25 00:13:01 -0500

    added code

--- lib/netpix.rb
+++ lib/netpix.rb
@@ -1,9 +1,15 @@
-module NetPix
-  class Print_Picture
+require 'mini_magick'
+require 'catpix'
 
-    def hello
-      puts "testing 1 2 3"
+module NetPix
+  class Print_Pix
+
+    def pic_from_link(linky)
+      image = MiniMagick::Image.open(linky)
+      Catpix::print_image image.path
     end
     
   end
 end    
+
+NetPix::Print_Pix.new.pic_from_link('https://i.imgur.com/VsxYwz0.jpg')
